540e3f38dc486407601c72fd2f3077645043343e
LHWOOD <hutsonwood@gmail.com>
2021-11-09 15:08:21 -0700

    changed the file structure

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const stuff = "hi";
+const room = document.querySelector("#fish");
 
-console.log(stuff);
+const button = document.querySelector("#btn");
+
+button.addEventListener("click", function () {
+  fetch("https://api.fungenerators.com", {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+});
