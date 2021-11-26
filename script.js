a5a52a8076ca38654e8b116d10e0ae389c60cfc1
LHWOOD <hutsonwood@gmail.com>
2021-11-26 15:46:00 -0700

    created display cards and valuesj

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const room = document.querySelector("#fish");
 
 const button = document.querySelector("#btn");
 
+// const one = "1";
+// const two = "2";
+// const three = "3";
+// const four = "4";
+// const five = "5";
+// const six = "6";
+// const seven = "7";
+// const eight = "8";
+// const nine = "9";
+// const ten = "10";
+// const jack = "11";
+// const queen = "12";
+// const king = "13";
+// const ace = "14";
+
 button.addEventListener("click", function () {
   fetch(
     // "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=DU7T0IAOA31JGVC6",
@@ -18,24 +33,37 @@ button.addEventListener("click", function () {
     .then(function (data) {
       console.log(data);
 
-      button.setAttribute("style", "display: none");
+      // button.setAttribute("style", "display: none");
 
       let card1 = document.createElement("img");
       let card2 = document.createElement("img");
 
-      let cardInfo = document.createElement("p");
-
       let test1 = data.cards[0].image;
       let test2 = data.cards[1].image;
-      cardInfo.setAttribute("style", "color: white");
+
       card1.setAttribute("src", test1);
-      cardInfo.textContent = data.cards[0].value;
+
+      let cardValue1 = data.cards[0].value;
+      let cardValue2 = data.cards[1].value;
 
       card2.setAttribute("src", test2);
 
       room.appendChild(card1);
-      room.appendChild(cardInfo);
       room.appendChild(card2);
+
+      if (cardValue1 > cardValue2) {
+        let winner = document.createElement("h1");
+        winner.textContent = "You Won!";
+        room.appendChild(winner);
+      } else if (cardValue1 < cardValue2) {
+        let loser = document.createElement("h1");
+        loser.textContent = "You Lost!";
+        room.appendChild(loser);
+      } else if (cardValue1 === cardValue2) {
+        let tie = document.createElement("h1");
+        tie.textContent = "You Tied!";
+        room.appendChild(tie);
+      }
     });
 });
 
